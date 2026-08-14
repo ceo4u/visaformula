8ed1d2ba1e90604d8fb2b477133dfb203feb7b3b
ceo4u <manager.ceo5.co@gmail.com>
2026-08-14 11:59:13 +0530

    Fix Vite SSR ReferenceError module is not defined by reverting manual react path alias

--- astro.config.mjs
+++ astro.config.mjs
@@ -30,8 +30,6 @@ export default defineConfig({
       dedupe: ['react', 'react-dom', 'react/jsx-runtime', 'react/jsx-dev-runtime'],
       alias: {
         '@': path.resolve(__dirname, './src'),
-        'react': path.resolve(__dirname, './node_modules/react'),
-        'react-dom': path.resolve(__dirname, './node_modules/react-dom'),
       },
     },
     optimizeDeps: {
